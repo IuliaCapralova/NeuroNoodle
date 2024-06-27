scaling fixed

Logistic_Regression/logistic_regression.py
@@ -25,7 +25,7 @@ class LogisticRegression():
             self.intercept = w[0]
             self.weights = w[1:]
 
-            # Calculate predictions, gradient and loss and BCE (problems with this haha (for now))
+            # Calculate predictions, gradient, loss, and BCE
             y_pred = self.predict(X)
             y_pred = np.array(y_pred)
             gradient_w, gradient_b = self._gradient(n_data_points, X, y, y_pred)

PCA/pca_class.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 
 class PCA_1:
@@ -15,7 +16,6 @@ class PCA_1:
         sd = np.std(X, axis=0, ddof=1)   # find sd of each feature
         # data centering
         X = X - mean
-        X = X / sd
         return X
 
     def _train(self, X: np.array):