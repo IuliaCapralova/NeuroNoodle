3a3caadd551c53e4785348bd0319c782e9d34488
Iulia Capralova <i.capralova@student.rug.nl>
2024-06-30 22:34:42 +0200

    train test split changed

--- Logistic_Regression/main.py
+++ Logistic_Regression/main.py
@@ -24,7 +24,7 @@ def main():
     X = df.drop('diagnosis', axis = 1)
     y = df.diagnosis
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
     # scale dara
     scaler = MinMaxScaler()
